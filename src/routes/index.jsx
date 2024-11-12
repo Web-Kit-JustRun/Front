01879620d1868c65f891eaf0ca57ca2b457fbaeb
Heemin2be <heemin7904@gmail.com>
2024-11-12 16:13:36 +0900

    랭크 버튼 추가

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import SearchId from "../pages/Login/SearchId";
 import SearchPw from "../pages/Login/SearchPw";
 import Register from "../pages/Login/Register";
 import Main from "../pages/Main";
+import Rank from "../pages/Rank";
 
 //기본 틀임. 로그인 회원가입은 고정된 레이아웃이 없을거라 여기(사실 역할별로 나눈거)
 const publicRoutes = [
@@ -20,9 +21,16 @@ const publicRoutes = [
     path: "/register",
     element: Register,
   },
+  {
+    path: "/rank",
+    element: Rank,
+  },
 ];
 
-const authenticatedRoutes = [{ path: "/main", element: Main }];
+const authenticatedRoutes = [
+  { path: "/main", element: Main },
+  { path: "/rank", element: Rank },
+];
 
 // 추가로 필요한게 생각나면 위 형식대로 만들면 됨
 export { publicRoutes, authenticatedRoutes }; // 여기서 export해주는거 까먹으면 오류남
