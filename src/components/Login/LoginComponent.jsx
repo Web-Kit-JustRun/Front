b409e4829e485a893b6cd22f887488466568ae93
kth5352 <kth5352@gmail.com>
2024-11-20 17:41:12 +0900

    inputForm lable 변경

--- src/components/Login/LoginComponent.jsx
+++ src/components/Login/LoginComponent.jsx
@@ -28,22 +28,24 @@ const LoginComponent = (props) => {
           <h1>로그인</h1>
           <Form>
             <InputWrapper>
-              <Label>아이디</Label>
               <InputForm
+                title={"아이디"}
                 eMsgColor={idError ? "red" : "white"}
                 eMsgContent={idError ? "아이디를 입력해 주세요" : ""}
                 onChange={handleIdChange}
                 value={userId}
+                inputTitle={"아이디를 입력해주세요"}
               />
             </InputWrapper>
             <InputWrapper>
-              <Label>비밀번호</Label>
               <InputForm
+                title={"비밀번호"}
                 type="password"
                 eMsgColor={pwError ? "red" : "white"}
                 eMsgContent={pwError ? "비밀번호를 입력해 주세요" : ""}
                 onChange={handlePwChange}
                 value={userPw}
+                inputTitle={"비밀번호를 입력해주세요"}
               />
             </InputWrapper>
             <ActionButton onClick={handleFormSubmit}>로그인</ActionButton>
